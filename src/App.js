41d75754e59b946d63b59ea6edb68db07a76de0e
Ben <ben.austin@melodysquare.com>
2022-02-12 17:30:20 +0900

    First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import SecretWord from './components/SecretWord'
+import SecretWordFeedback from './components/SecretWordFeedback'
+import TextBar from './components/TextBar'
+import word_list from './assets/word_list.json'
+import { DISPLAY_OPTIONS } from './assets/constants.js'
 
 function App() {
+  const [value, setValue] = useState()
+  const [randomizedWord, setRandomizedWord] = useState()
+  const [points, setPoints] = useState(0)
+  const [displayed, setDisplayed] = useState(DISPLAY_OPTIONS[0])
+
+  const randomizeWord = () => {
+    let randomNumber = Math.floor(Math.random() * word_list.length)
+    setRandomizedWord(word_list[randomNumber])
+    setDisplayed(DISPLAY_OPTIONS[0])
+  }
+
+  const increasePoints = () => {
+    setPoints(points + 1)
+  }
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      console.log('Hey there')
+    }
+  }
+
+  useEffect(() => {
+    randomizeWord()
+  }, [])
+
+  const compareText = (inputText, randomWord) => {
+    if (inputText === randomWord) {
+      increasePoints()
+      setValue('')
+      setDisplayed(DISPLAY_OPTIONS[1])
+    }
+  }
+
+  useEffect(() => {
+    if (randomizedWord) {
+      compareText(value, randomizedWord.word[0].romaji)
+    }
+  })
+
+  const handleChange = (event) => {
+    setValue(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' onKeyDown={handleKeyDown}>
+      {randomizedWord && displayed === 'RANDOM_WORD' ? (
+        <>
+          <SecretWord randomizedWord={randomizedWord.word[0].japanese} />
+          <TextBar value={value} handleChange={handleChange} />
+        </>
+      ) : (
+        <></>
+      )}
+      {randomizedWord && displayed === 'FEEDBACK' ? (
+        <SecretWordFeedback
+          randomizedWord={randomizedWord}
+          randomizeWord={randomizeWord}
+        />
+      ) : (
+        <></>
+      )}
+      <h3>Points: {points}</h3>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/assets/DONTREADME.md
@@ -0,0 +1,8 @@
+- make za, ha, ba, pa, etc. work. via button? i guess so
+- FIRST you want japanese word, with english typing only. So that simplifies your beta version. Don't program keyboard.
+- Make a project map
+
+- make enter submit the "OK" button
+- expand katakana list. look into way of automizing upload
+- create alternate game with matching the definition
+  - with this, get definition to be an array (which would cause changes elsewhere)
